b59bee4b89553066309ed58546bcc03679a4f48b
nachat-ayoub <ayoub.nachat.27@gmail.com>
2024-04-30 02:05:55 +0100

    home functionality 80%

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+import { getHomePage } from './scraper';
+
+import HomeSlider from '@/components/home-slider';
+
 export default async function Home() {
+  const data = await getHomePage();
+
   return (
     <main className=''>
+      <section className=''>
+        <HomeSlider featured_episodes={data.featured_episodes} />
+      </section>
+
       <div className='flex min-h-screen flex-col items-center justify-between p-24'>
-        <h1 className='text-3xl font-bold underline'>Welcom To Anime Sphere!</h1>
+        <h1 className='text-3xl font-bold underline'>
+          Welcom To Anime Sphere!
+        </h1>
       </div>
     </main>
   );
